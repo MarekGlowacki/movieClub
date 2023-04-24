816985b34b7e8bbaec918cebe7718bc36e3a0edc
User <user@its.me>
2023-04-24 12:55:39 +0200

    filling test data in dev profile

---
+++ src/main/resources/db/testdata/0001_genres.sql
@@ -0,0 +1,10 @@
+insert into genre (name) values ('Comedy|Drama|Romance');
+insert into genre (name) values ('Comedy|Horror|Musical|Sci-Fi');
+insert into genre (name) values ('Comedy');
+insert into genre (name) values ('Comedy|Romance');
+insert into genre (name) values ('Comedy|Drama');
+insert into genre (name) values ('Action|Drama');
+insert into genre (name) values ('Action|Sci-Fi');
+insert into genre (name) values ('Drama');
+insert into genre (name) values ('Crime|Drama|Mystery|Romance|Thriller');
+insert into genre (name) values ('Documentary');
