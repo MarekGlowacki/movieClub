ae580bd1cd815755957af8700d4cbe4eef272494
User <user@its.me>
2023-05-01 11:48:05 +0200

    user registered notification

--- src/main/java/online/javafun/movieclub/web/RegistrationController.java
+++ src/main/java/online/javafun/movieclub/web/RegistrationController.java
@@ -2,10 +2,12 @@ package online.javafun.movieclub.web;
 
 import online.javafun.movieclub.domain.user.UserService;
 import online.javafun.movieclub.domain.user.dto.UserRegistrationDto;
+import online.javafun.movieclub.web.admin.AdminController;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class RegistrationController {
@@ -23,8 +25,11 @@ public class RegistrationController {
     }
 
     @PostMapping("/registration")
-    public String register(UserRegistrationDto userRegistration) {
+    public String register(UserRegistrationDto userRegistration, RedirectAttributes redirectAttributes) {
         userService.registerUserWithDefaultRole(userRegistration);
+        redirectAttributes.addFlashAttribute(
+                AdminController.NOTIFICATION_ATTRIBUTE,
+                "Username %s saved".formatted(userRegistration.getEmail()));
         return "redirect:/";
     }
 }
