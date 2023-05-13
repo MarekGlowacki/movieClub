9da8cec076f32b2d9c969b27f6168f0ff99755dc
User <user@its.me>
2023-05-13 17:24:07 +0200

    remove error when image not started yet

--- build-and-restart.bat
+++ build-and-restart.bat
@@ -1,4 +1,4 @@
 docker build . -t movieclub
-docker stop movieclub
-docker rm movieclub
+docker stop movieclub || true
+docker rm movieclub || true
 docker run -d -p 8080:8080 --name=movieclub movieclub
