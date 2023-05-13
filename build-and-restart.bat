51bdd7b398b4ef115aec6035e86fd9f89930628a
User <user@its.me>
2023-05-13 18:34:47 +0200

    add to network automatically

--- build-and-restart.bat
+++ build-and-restart.bat
@@ -1,4 +1,4 @@
 docker build . -t movieclub
 docker stop movieclub || true
 docker rm movieclub || true
-docker run -d -p 8080:8080 --name=movieclub movieclub
+docker run -d -p 8080:8080 --name=movieclub --network NETWORK movieclub
