037e688464ef9f71eb24eaef0259ebea693bbb35
User <user@its.me>
2023-05-14 09:30:13 +0200

    movies description translate to english

--- src/main/resources/db/testdata/0001_genres.sql
+++ src/main/resources/db/testdata/0001_genres.sql
@@ -1,10 +1,5 @@
-insert into genre (name, description) values ('Drama', 'Integer ac leo. Pellentesque ultrices mattis odio. Donec vitae nisi.');
-insert into genre (name, description) values ('Comedy', 'Duis mattis egestas metus. Aenean fermentum. Donec ut mauris eget massa tempor convallis.');
-insert into genre (name, description) values ('Romance', 'Integer aliquet, massa id lobortis convallis, tortor risus dapibus augue, vel accumsan tellus nisi eu orci. Mauris lacinia sapien quis libero. Nullam sit amet turpis elementum ligula vehicula consequat.');
-insert into genre (name, description) values ('Thriller', 'Phasellus sit amet erat. Nulla tempus.');
-insert into genre (name, description) values ('Documentary', 'Nulla ac enim. In tempor, turpis nec euismod scelerisque, quam turpis adipiscing lorem, vitae mattis nibh ligula nec sem. Duis aliquam convallis nunc.');
-insert into genre (name, description) values ('Action', 'Nulla tempus. Vivamus in felis eu sapien cursus vestibulum.');
-insert into genre (name, description) values ('History', 'Integer pede justo, lacinia eget, tincidunt eget, tempus vel, pede. Morbi porttitor lorem id ligula.');
-insert into genre (name, description) values ('Sci-Fi', 'Praesent blandit lacinia erat. Vestibulum sed magna at nunc commodo placerat. Praesent blandit.');
-insert into genre (name, description) values ('Musical', 'Sed ante. Vivamus tortor.');
-insert into genre (name, description) values ('Horror', 'Nullam porttitor lacus at turpis. Donec posuere metus vitae ipsum. Aliquam non mauris.');
+insert into genre (name, description)
+values ('Action', 'Action films are fast, energetic, and spectacular. The central character is a protagonist who fights against evil. The integral part scenes are battles, chase scenes, rescues, escapes, and fights.'),
+       ('Fantasy', 'Fantasy films immerse audiences in non-existent universes filled with limitless opportunities. Escapism, dreams, and wonders serve as tools to open new boundaries of physical laws or human possibilities.'),
+       ('Drama', 'Probably the largest film category. Dramas portray people and their inner selves; they depict the characters from different sides and show their personal growth. Dramas illustrate characters and their unique stories in a real-life setting.'),
+       ('Animation', 'Histories not only for youngest');
