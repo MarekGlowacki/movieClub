9531505e22cc65dc95902443282e4abab0cd4a25
User <user@its.me>
2023-10-10 07:27:53 +0200

    use database!

--- build-and-restart.bat
+++ build-and-restart.bat
@@ -1,3 +1,3 @@
 docker build . -t movieclub
 
-docker run -d -p 8080:8080 --name=movieclub movieclub
+docker run -d -p 8080:8080 --name=movieclub movieclub -e SPRING_PROFILES_ACTIVE=prod --network NETWORK movieclub --restart unless-stopped movieclub
