6d705ccd8fd0e827081f160e28a594223ec6f8d1
User <user@its.me>
2023-10-10 07:30:04 +0200

    use network!

--- build-and-restart.bat
+++ build-and-restart.bat
@@ -1,3 +1,3 @@
 docker build . -t movieclub
 
-docker run -d -p 8080:8080 --name=movieclub movieclub -e SPRING_PROFILES_ACTIVE=prod --network NETWORK movieclub --restart unless-stopped movieclub
+docker run -d -p 8080:8080 --name=movieclub movieclub -e SPRING_PROFILES_ACTIVE=prod --network movieclub-network --restart unless-stopped movieclub
