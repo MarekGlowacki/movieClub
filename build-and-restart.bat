5c1767defe89e364894fb3ff311c5e18a5a367d9
User <user@its.me>
2024-03-19 13:20:43 +0200

    remove restart

--- build-and-restart.bat
+++ build-and-restart.bat
@@ -1,4 +1,4 @@
 docker build . -t movieclub
 docker stop movieclub || true
 docker rm movieclub || true
-docker run -d -p 8080:8080 --name=movieclub -e SPRING_PROFILES_ACTIVE=prod --network movieclub-network --restart unless-stopped movieclub
+docker run -d -p 8080:8080 --name=movieclub -e SPRING_PROFILES_ACTIVE=prod --network movieclub-network movieclub
