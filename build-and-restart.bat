8f128321f9ddcc49cdfc74d9fe6837cf8532f507
User <user@its.me>
2024-03-19 13:31:21 +0200

    back to previous name

--- build-and-restart.bat
+++ build-and-restart.bat
@@ -1,4 +1,4 @@
 docker build . -t movieclub
 docker stop movieclub || true
 docker rm movieclub || true
-docker run -d -p 8080:8080 --name=movieclubb -e SPRING_PROFILES_ACTIVE=prod --network movieclub-network movieclub
+docker run -d -p 8080:8080 --name=movieclub -e SPRING_PROFILES_ACTIVE=prod --network movieclub-network movieclub
